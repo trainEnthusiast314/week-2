9530aa9994a902f13aef87dd9786b8c230c402a5
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-01-29 10:56:29 +0000

    task10

--- task10/index.js
+++ task10/index.js
@@ -1,11 +1,27 @@
 // Set up code - do not remove
-document.querySelector('button#textColour').addEventListener('click', blueToRed);
-document.querySelector('button#bgColour').addEventListener('click', greenToPink);
-document.querySelector('button#fonts').addEventListener('click', tnrToArial);
+document
+  .querySelector("button#textColour")
+  .addEventListener("click", blueToRed);
+document
+  .querySelector("button#bgColour")
+  .addEventListener("click", greenToPink);
+document.querySelector("button#fonts").addEventListener("click", tnrToArial);
 // Do not alter lines of code 2-4!
+const blueParagraph = document.querySelector("#blueParagraph");
 
-function blueToRed() {}
+function blueToRed() {
+  blueParagraph.style.color = "red";
+  console.log(blueParagraph.style.color);
+  blueParagraph.textContent = blueParagraph.textContent.replace("blue", "red");
+  console.dir(blueParagraph);
+  blueParagraph.style.fontFamily = "sans-serif";
+}
+const greenParagraphs = document.querySelectorAll(".greenBg");
 
-function greenToPink() {}
+function greenToPink() {
+  for (let i = 0; i < greenParagraphs.length; i++) {
+    greenParagraphs[i].className = "hotpinkBg";
+  }
+}
 
 function tnrToArial() {}
