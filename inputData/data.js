1c2411a84caa327f8a73edc75c995d5a3ba0968d
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-01-30 14:28:37 +0000

    display list

---
+++ inputData/data.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userForm = document.querySelector("#userForm");
+
+  const data = {};
+  const dataBind = () => {};
+  dataBind.get = (key) => {
+    return data[key];
+  };
+  dataBind.set = (key, value) => {
+    data[key] = value;
+  };
+  dataBind.display = () => {
+    document.querySelectorAll("[username], [data]").forEach((item) => {
+      if (item.username == undefined) {
+        let key = item.getAttribute("data");
+
+        item.innerText = data[key];
+      } else {
+        item.value = data[item.username];
+      }
+    });
+  };
+  dataBind.list = () => {
+    console.log(data);
+    const newUl = document.createElement("ul");
+    const newLi = document.createElement("li");
+    const content = document.createTextNode(
+      `USERNAME: ${data["username"]}     AGE: ${data["age"]}`
+    );
+    const filledLi = newLi.appendChild(content);
+    newUl.appendChild(filledLi);
+
+    document.querySelector("#container").appendChild(newUl);
+  };
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const username = document.getElementsByName("username")[0];
+    const age = document.getElementsByName("age")[0];
+    if (/^\d+$/.test(age.value) && /^[a-zA-Z]+$/gim.test(username.value)) {
+      dataBind.set("username", username.value);
+      dataBind.set("age", age.value);
+      //dataBind.display();
+      dataBind.list();
+    } else {
+      alert("invalid age or name");
+    }
+  });
+});
