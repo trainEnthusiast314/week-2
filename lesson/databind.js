a70c5b25e092f789f907f5b9b4ce97e902c7d4f0
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-02-01 14:36:02 +0000

    lesson

--- lesson/databind.js
+++ lesson/databind.js
@@ -1,46 +1,44 @@
-if(data == undefined){
-    data = {};
+if (data == undefined) {
+  data = {};
 }
 
+let dataBind = function () {};
+dataBind.get = function (key) {
+  let field = data;
+  key.split(".").forEach((item) => {
+    field = field[item];
+  });
+  console.log(key);
+  return field;
+};
+dataBind.set = function (key, value) {
+  data[key] = value;
+};
 
-let dataBind = function(){
+dataBind.display = function () {
+  document.querySelectorAll("[data]").forEach(function (item) {
+    if (item.name == undefined) {
+      let key = item.getAttribute("data");
+      if (dataBind.get(key) !== undefined) {
+        item.innerText = dataBind.get(key);
+      }
+    } else {
+      let key = item.name;
+      if (dataBind.get(key) !== undefined) {
+        item.value = dataBind.get(key);
+      }
+    }
+  });
+};
 
-}
-dataBind.get = function(key){
-    return data[key];
-}
-dataBind.set = function(key,value){
-    data[key] = value;
-}
-
-dataBind.display = function(){
-    document.querySelectorAll('[name], [data]').forEach(function(item){
-        if(item.name == undefined){
-            let key = item.getAttribute("data");
-            if(data[key] !== undefined){
-                item.innerText = data[key];
-            }
-        }
-        else{
-            let key = item.name;
-            if(data[key] !== undefined){
-                item.value = data[key];
-            }
-        }
-    });
-}
-
-
-dataBind.save = function(){
-    console.log( JSON.stringify(data)  )
-    sessionStorage.setItem('data', JSON.stringify(data) );
-}
-
-
+dataBind.save = function () {
+  console.log(JSON.stringify(data));
+  sessionStorage.setItem("data", JSON.stringify(data));
+};
 
 dataBind.display();
 
-document.addEventListener("input", function(e){
-    dataBind.set(e.target.name, e.target.value);
-    dataBind.display();
-})
+document.addEventListener("input", function (e) {
+  dataBind.set(e.target.name, e.target.value);
+  dataBind.display();
+});
---
+++ lesson/export.js
@@ -0,0 +1,8 @@
+const object = {
+  stuff: [1, 2, 3, 75, 123],
+  action: () => {
+    alert(123);
+  },
+};
+
+export default object;
